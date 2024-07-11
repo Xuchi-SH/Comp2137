9b6e5ab3c6c4f80d5338d4168745f8caec6f571f
Xuchi-SH <xuchi9606@gmail.com>
2024-07-11 13:42:26 -0400

    user recreate

--- assignment2.sh
+++ assignment2.sh
@@ -108,24 +108,28 @@ public_keys["dennis"]="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG4rT3vTt99Ox5kndS4Hm
 
 # Create users with home directory in /home and bash as default shell
 for username in "${usernames[@]}"; do
-	log "Create $username"
-    useradd -m -d "/home/$username" -s /bin/bash "$username"
+	if id -u "$username" >/dev/null 2>&1; then
+		log "User $username already exists, skipping creation."
+	else
+		log "Create $username"
+    	useradd -m -d "/home/$username" -s /bin/bash "$username"
 
 # Generate SSH keys (RSA and Ed25519) for each user
-	log "ssh-keygen $username"
-    su - "$username" -c "ssh-keygen -q -t rsa -b 4096 -f ~/.ssh/id_rsa -N ''"
-    su - "$username" -c "ssh-keygen -q -t ed25519 -f ~/.ssh/id_ed25519 -N ''"
+		log "ssh-keygen $username"
+    	su - "$username" -c "ssh-keygen -q -t rsa -b 4096 -f ~/.ssh/id_rsa -N ''"
+    	su - "$username" -c "ssh-keygen -q -t ed25519 -f ~/.ssh/id_ed25519 -N ''"
 
 # Add public keys to authorized_keys file
-	log "public key authorized_keys $username"
-    cat "/home/$username/.ssh/id_rsa.pub" >> "/home/$username/.ssh/authorized_keys"
-    cat "/home/$username/.ssh/id_ed25519.pub" >> "/home/$username/.ssh/authorized_keys"
-	if [ "$username" == "dennis" ]; then
-		echo "${public_keys[$username]}" >> "/home/$username/.ssh/authorized_keys"	
-		usermod -aG sudo $username
+		log "public key authorized_keys $username"
+    	cat "/home/$username/.ssh/id_rsa.pub" >> "/home/$username/.ssh/authorized_keys"
+    	cat "/home/$username/.ssh/id_ed25519.pub" >> "/home/$username/.ssh/authorized_keys"
+		if [ "$username" == "dennis" ]; then
+			echo "${public_keys[$username]}" >> "/home/$username/.ssh/authorized_keys"	
+			usermod -aG sudo $username
+		fi
+		chmod 600 /home/$username/.ssh/authorized_keys
+		chown -R $username:$username /home/$username/.ssh
 	fi
-	chmod 600 /home/$username/.ssh/authorized_keys
-	chown -R $username:$username /home/$username/.ssh
 done
 
 log "User accounts created successfully!"

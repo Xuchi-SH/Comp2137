a766e1dcbba307fa9cba72dfe3ca1e84980396f4
Xuchi-SH <xuchi9606@gmail.com>
2024-07-16 13:54:40 -0400

    Assignmment2 checked

---
+++ Assign03/lab3.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# This script runs the configure-host.sh script from the current directory to modify 2 servers and update the local /etc/hosts file
+
+scp configure-host.sh remoteadmin@server1-mgmt:/root
+ssh remoteadmin@server1-mgmt -- /root/configure-host.sh -name loghost -ip 192.168.16.3 -hostentry webhost 192.168.16.4
+
+name1=$(ssh remoteadmin@server1-mgmt --  grep loghost /etc/hostname)
+name2=$(ssh remoteadmin@server1-mgmt --  hostname)
+name3=$(ssh remoteadmin@server1-mgmt --  grep loghost /etc/hosts|grep 192.168.16.3)
+ip1=$(ssh remoteadmin@server1-mgmt --  ip a | grep 192.168.16.3)
+ip2=$(ssh remoteadmin@server1-mgmt --  grep 192.168.16.4 /etc/hosts|grep webhost)
+
+echo "===================================Server1 Check============================================"
+echo "name1: $name1  name2: $name2"
+echo "name3: $name3"
+echo "ip1: $ip1"
+echo "ip2: $ip2"
+echo "check server1 finished"
+[[ -n "$name1" ]] && [[ "$name2" -eq "loghost" ]] && echo "hostname is right" || echo "hostname is wrong"
+[[ -n "$name3" ]] && echo "/etc/hosts is right" || echo "/etc/hosts is wrong"
+[[ -n "$ip1" ]] && echo "system ip address is right" || echo "system ip address in /etc/hostname is wrong"
+[[ -n "$ip2" ]] && echo "host entry in /etc/hosts is right" || echo "host entry  in /etc/hosts is wrong"
+
+
+echo
+echo
+
+scp configure-host.sh remoteadmin@server2-mgmt:/root
+ssh remoteadmin@server2-mgmt -- /root/configure-host.sh -name webhost -ip 192.168.16.4 -hostentry loghost 192.168.16.3
+
+name1=$(ssh remoteadmin@server2-mgmt --  grep webhost /etc/hostname)
+name2=$(ssh remoteadmin@server2-mgmt --  hostname)
+name3=$(ssh remoteadmin@server2-mgmt --  grep webhost /etc/hosts|grep 192.168.16.4)
+ip1=$(ssh remoteadmin@server2-mgmt --  ip a | grep 192.168.16.4)
+ip2=$(ssh remoteadmin@server2-mgmt --  grep 192.168.16.3 /etc/hosts|grep loghost)
+
+echo "===================================Server2 Check============================================"
+echo "name1: $name1  name2: $name2"
+echo "name3: $name3"
+echo "ip1: $ip1"
+echo "ip2: $ip2"
+echo "check server2 finished"
+
+[[ -n "$name1" ]] && [[ "$name2" -eq "webhost" ]] && echo "hostname is right" || echo "hostname is wrong"
+[[ -n "$name3" ]] && echo "/etc/hosts is right" || echo "/etc/hosts is wrong"
+[[ -n "$ip1" ]] && echo "system ip address is right" || echo "system ip address in /etc/hostname is wrong"
+[[ -n "$ip2" ]] && echo "host entry in /etc/hosts is right" || echo "host entry  in /etc/hosts is wrong"
+
+echo
+echo
+
+./configure-host.sh -hostentry loghost 192.168.16.3
+./configure-host.sh -hostentry webhost 192.168.16.4
+
+echo "===================================Host Check============================================"
+desired_name="loghost"
+desired_ip="192.168.16.3"
+entry=$(grep -E "^\s*$desired_ip\s+$desired_name(\s|$)" /etc/hosts)
+echo "$entry"
+[[ -n "$entry" ]] && echo "/etc/hosts contains loghost 192.168.16.3" || echo "/etc/hosts does not contain loghost 192.168.16.3"
+
+desired_name="webhost"
+desired_ip="192.168.16.4"
+entry=$(grep -E "^\s*$desired_ip\s+$desired_name(\s|$)" /etc/hosts)
+echo "$entry"
+[[ -n "$entry" ]] && echo "/etc/hosts contains webhost 192.168.16.4" || echo "/etc/hosts does not contain webhost 192.168.16.4"
+
+
+
+
+
+
